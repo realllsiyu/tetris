5ae45ae18da2cd6866fd5b638611e3c87355b006
realllsiyu <siyuc0712@gmail.com>
2022-02-04 09:39:06 -0500

    Created grids and tetrominoes

---
+++ src/script.js
@@ -0,0 +1,59 @@
+const grid = document.querySelector(".grid");
+const scoreDisplay = document.querySelector("#score");
+const startBtn = document.querySelector("#start-button");
+let squares = document.querySelectorAll(".grid");
+const width = 10;
+
+//The Tetrominoes
+const lTetromino = [
+  [1, width + 1, width * 2 + 1, 2],
+  [width, width + 1, width + 2, width * 2 + 2],
+  [1, width + 1, width * 2 + 1, width * 2],
+  [width, width * 2, width * 2 + 1, width * 2 + 2],
+];
+
+const zTetromino = [
+  [0, width, width + 1, width * 2 + 1],
+  [width + 1, width + 2, width * 2, width * 2 + 1],
+  [0, width, width + 1, width * 2 + 1],
+  [width + 1, width + 2, width * 2, width * 2 + 1],
+];
+
+const tTetromino = [
+  [1, width, width + 1, width + 2],
+  [1, width + 1, width + 2, width * 2 + 1],
+  [width, width + 1, width + 2, width * 2 + 1],
+  [1, width, width + 1, width * 2 + 1],
+];
+
+const oTetromino = [
+  [0, 1, width, width + 1],
+  [0, 1, width, width + 1],
+  [0, 1, width, width + 1],
+  [0, 1, width, width + 1],
+];
+
+const iTetromino = [
+  [1, width + 1, width * 2 + 1, width * 3 + 1],
+  [width, width + 1, width + 2, width + 3],
+  [1, width + 1, width * 2 + 1, width * 3 + 1],
+  [width, width + 1, width + 2, width + 3],
+];
+
+const theTetrominoes = [
+  lTetromino,
+  zTetromino,
+  tTetromino,
+  oTetromino,
+  iTetromino,
+];
+
+let currentPosition = 4;
+let current = theTetrominoes[0][0];
+function draw() {
+  current.forEach((index) => {
+    squares[currentPosition + index].classList.add("tetromino");
+  });
+}
+
+draw();
